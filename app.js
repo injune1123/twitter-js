cf192d6865cd836eaf321283789f2154a6ea4192
Jugal Shah <jugaltheshah@gmail.com>
2015-08-01 14:32:23 -0400

    Rendering template works!

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ var express = require("express");
 var morgan = require('morgan');
 var swig = require("swig");
 var app = express();
-
 app.use(morgan('dev'))
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.engine('swig',swig.renderFile);
+console.log("Before setting app parameters");
+app.engine('html',require("swig").renderFile);
 app.set('view engine',"html");
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views/');
+
+console.log("Before setting swig cache off");
 swig.setDefaults({ cache: false });
 
+console.log("Before app.get");
+app.get('/', function (req, res) {
+  var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render('index', {title: 'Hall of Fame', people: people});
+});
+
+console.log("Before server listening");
 var server = app.listen(3000,function(){
   var host = server.address().address;
   var port = server.address().port;

a821437c6d48454e74861a18c1ff79aed13edab0
Jun Yin <injune1123@Juns-Air.home>
2015-08-03 22:03:12 -0400

    added router by ID under the helpof Jugal

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,26 @@ var tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+  //console.log("Tweets: " + tweets);
+  res.render( 'index', {title: 'Twitter.js', tweets: tweets } );
+});
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find({name: name});
+  console.log("List length: " + list.length);
+  res.render( 'index', {title: 'Twitter.js - Posts by '+name, tweets: list});
+});
+
+router.get('/users/:name/tweets/:id', function(req, res) {
+  var name = req.params.name;
+  var reqID = 1*req.params.id;
+  //var nameID = "";
+  //nameID = name.replace(/\s+/g, '') + reqID;
+  var nameList = tweetBank.find({name: name})
+  var list = tweetBank.subFind(nameList, {id: reqID});
+  //console.log("Found list: " + list.length);
+  res.render('index', {title: 'Twitter.js - Posts by ' + name, tweets: list});
 });
 
 module.exports = router;
